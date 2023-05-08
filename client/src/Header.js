d95ebc04719d5b382a9b95c3fc52ef77102e58de
muneeb202 <118543795+muneeb202@users.noreply.github.com>
2023-05-08 12:38:07 +0500

    Update Header.js

--- client/src/Header.js
+++ client/src/Header.js
@@ -40,6 +40,7 @@ function SideBar() {
   const navigate = useNavigate();
 
   const configuration = new Configuration({
+    apiKey: "",
     headers: {
       "User-Agent":
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
